14df9a05acefe958a430ffdfc2ade3b518b83e69
swon95 <shimtkddnjs13@gmail.com>
2024-01-24 17:17:38 +0900

    feat: status code

--- controllers/brand-controller.js
+++ controllers/brand-controller.js
@@ -4,7 +4,7 @@ module.exports = {
   getBrand: async (req, res) => {
     try {
       const data = await brandService.getBrandData();
-      res.json({ data });
+      res.status(200).json({ data });
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
