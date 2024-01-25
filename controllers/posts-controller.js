64bf3d4668436355d5a5123ca391f6036111a869
hookor <gerrard1219@gmail.com>
2024-01-25 19:33:04 +0900

    Feat: postRegister

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -1,12 +1,28 @@
 const PostService = require('../services/post-service');
 // 1. 포스트 조회
 exports.getPostDetail = async (req, res) => {
-  const { postReq } = req.params.postId;
+  const postReq = req.params.postId;
   const postRes = await PostService.getPostDetail(postReq).catch(err => {
-    console.log(err);
+    console.log('Controller:', err);
     res.status(500).json({
-      message: err
+      error: err
     });
   });
-  return res.status(200).json({ postRes });
+  return (
+    postRes &&
+    res.status(200).json({ data: postRes !== undefined ? postRes : null })
+  );
+};
+// 2. 포스트 등록
+exports.registerPost = async (req, res) => {
+  const postRes = await PostService.registerPost(req).catch(err => {
+    console.log('Controller:', err);
+    res.status(500).json({
+      error: err
+    });
+  });
+  return (
+    postRes &&
+    res.status(200).json({ success: postRes !== undefined ? 'ok' : null })
+  );
 };
--- services/post-service.js
+++ services/post-service.js
@@ -2,6 +2,13 @@ const postsDB = require('../models/posts-db');
 
 module.exports = {
   getPostDetail: async (req, res) => {
-    await postsDB.getPostDetail(req);
+    const result = await postsDB.getPostDetail(req);
+    return result;
+  },
+  registerPost: async (req, res) => {
+    const result = req.body
+      ? await postsDB.registerPost(req.body)
+      : Promise.reject('Invalid Request');
+    return result;
   }
 };
