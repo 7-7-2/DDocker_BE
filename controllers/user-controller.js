88cf5cb5fdadc610da3ec2768218db16e5c593c5
Siwoo Lee <siwoo28@kakao.com>
2024-01-30 15:02:10 +0900

    Refactor: router queryParams 추가

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,44 +1,13 @@
 const userService = require('../services/user-service');
 
-// controllers => Request, Response를 담당
-
-/** EXAMPLE CONTROLLER
- *  async (req, res, next) => {
-    // 실질적인 라우터 레벨의 책임(req => )
-    const userDTO = req.body;
-
-    // 서비스 레이어로의 책임 이동
-    // 데이터 레이어로의 접근과 비즈니스 로직 추상화
-    const { user, company } = await UserService.Signup(userDTO);
-
-    // ( => res)
-    return res.json({ user, company });
- */
-
-/**
- * !! REQ =>
- * req.params : 이름 붙은 라우트 파라미터
- * req.query : GET 방식으로 넘어오는 쿼리 스트링 파라미터
- * req.body : POST 방식으로 넘어오는 파라미터(body-parser)
- *
- * !! RES =>
- * res.status(code) : HTTP 응답 코드
- * res.send(body), res.send(status, body) : 클라이언트에 보내는 응답. 상태 코드는 옵션.
- *    기본 콘텐츠 타입은 text/html, text/plain을 보내려면 res.set(‘Content-Type’, ‘text/plain’)을 먼저 호출.
- *    JSON을 보낼거면 res.json
- * res.json(json), res.json(status, json) : 클라이언트로 JSON 값
- * res.sendFile(path, [options], [callback]) : path의 파일을 읽고 해당 내용을 클라이언트로 전송.
- */
-
 const signIn = async (req, res) => {
-  await userService.googleSignIn(res);
+  const { social } = req.query;
+
+  if (social === 'google') await userService.googleSignIn(res);
+  if (social === 'kakao') await userService.kakaoSignIn(res);
 };
 
-const kakaoSignIn = async (req, res) => {
-  await userService.kakaoSignIn(res);
-};
-
-const signInRedirect = async (req, res) => {
+const googleRedirect = async (req, res) => {
   const { code } = await req.query;
   try {
     const userInfo = await userService.getGoogleAuth(code);
@@ -83,11 +52,17 @@ const getUserInfo = async (req, res) => {
   return res.status(200).json(userInfo);
 };
 
+// middleware 적용
+const test = async (req, res) => {
+  // middleware에서 req.[name]으로 보낸 JWT payload
+  await res.status(200).json(`${req.userId}, ${req.nickname}`);
+};
+
 module.exports = {
+  test,
   signIn,
-  kakaoSignIn,
   kakaoRedirect,
-  signInRedirect,
+  googleRedirect,
   setInitForm,
   getUserInfo
 };
--- models/user-db.js
+++ models/user-db.js
@@ -1,32 +1,38 @@
 const db = require('../loaders/db');
 
 const setUserOauth = async req => {
-  console.log(req);
   const sql = 'INSERT INTO user (profileUrl, useremail, social) VALUES (?,?,?)';
   const conn = await db();
-  await conn.query(sql, req).catch(err => console.log(err));
+  const getConn = await conn.getConnection();
+  await getConn.query(sql, req).catch(err => console.log(err));
+  getConn.release();
 };
 
 const setUserInit = async req => {
   const sql =
     'UPDATE user SET nickname=?, gender=?, brand=?, sum = 0 WHERE id = ?';
   const conn = await db();
-  await conn.query(sql, req).catch(err => console.log(err));
+  const getConn = await conn.getConnection();
+  await getConn.query(sql, req).catch(err => console.log(err));
+  getConn.release();
 };
 
 const getUserInfo = async req => {
-  const sql = 'SELECT nickname, brand, sum FROM user WHERE id = ?';
+  const sql = 'SELECT id, nickname, brand, sum FROM user WHERE id = ?';
   const conn = await db();
-  const resault = await conn.query(sql, req).catch(err => console.log(err));
+  const getConn = await conn.getConnection();
+  const resault = await getConn.query(sql, req).catch(err => console.log(err));
+  getConn.release();
   return resault[0][0]['nickname'] ? resault : null;
 };
 
 const getUserAuthInfo = async req => {
   const sql = 'SELECT COUNT(*), id FROM user WHERE useremail = ? AND social= ?';
   const conn = await db();
+  const getConn = await conn.getConnection();
   const resault = await conn.query(sql, req).catch(err => console.log(err));
   const signInInfo = resault[0][0];
-  console.log(resault[0][0]['COUNT(*)']);
+  getConn.release();
   return signInInfo['COUNT(*)'] ? signInInfo['id'] : null;
 };
 
