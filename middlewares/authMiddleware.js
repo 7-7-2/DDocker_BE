91fc651cbe44f83681de661a7f1f9b8c4f03e5f2
Siwoo Lee <siwoo28@kakao.com>
2024-01-30 15:00:45 +0900

    Feat: authMiddleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+const { verify } = require('jsonwebtoken');
+require('dotenv').config();
+const { ACCESS_TOKEN_SECRET } = require('../config/index');
+
+const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token === null) return res.status(401).send('Unauthorized');
+
+  verify(token, ACCESS_TOKEN_SECRET, async (err, user) => {
+    if (err) return res.status(403).json('Forbidden');
+    // JWT Payload를 middleware => 사용하고하고자 하는 컨트롤러로 req.[name] 전달
+    req.userId = user.userId;
+    req.nickname = user.nickname;
+    next();
+  });
+};
+
+module.exports = { verifyToken };
