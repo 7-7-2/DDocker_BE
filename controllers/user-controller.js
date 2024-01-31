91a3867af0a48f6ef7b871a99bbb0c6cda69aa66
Siwoo Lee <siwoo28@kakao.com>
2024-01-31 16:46:33 +0900

    Fix: DDocker accessToken 구조 변경

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -10,9 +10,7 @@ const googleRedirect = async (req, res) => {
   const { code } = await req.query;
   try {
     const userInfo = await userService.getGoogleAuth(code);
-    const userId = await userService.setUserOauth(userInfo);
-    const resault = await userService.getAccessToken(userId);
-    if (!resault) return res.status(201).json({ id: userId });
+    const resault = await userService.setUserOauth(userInfo);
     return res.status(200).json({ accessToken: resault });
   } catch (error) {
     console.error('Error during authentication:', error.message);
@@ -24,9 +22,7 @@ const kakaoRedirect = async (req, res) => {
   const { code } = await req.query;
   try {
     const userInfo = await userService.getKakaoAuth(code);
-    const userId = await userService.setUserOauth(userInfo);
-    const resault = await userService.getAccessToken(userId);
-    if (!resault) return res.status(201).json({ id: userId });
+    const resault = await userService.setUserOauth(userInfo);
     return res.status(200).json({ accessToken: resault });
   } catch (error) {
     console.error('Error during authentication:', error.message);
@@ -35,16 +31,20 @@ const kakaoRedirect = async (req, res) => {
 };
 
 const setInitForm = async (req, res) => {
-  const body = await req.body;
-  const userId = Number(body.id);
-  const initReq = [body.nickName, body.gender, body.brand, userId];
-  await userService.setUserInit(initReq);
-  const accessToken = await userService.getAccessToken(userId);
-  return accessToken && res.status(200).json({ accessToken: accessToken });
+  try {
+    const body = await req.body;
+    const initReq = [body.nickName, body.gender, body.brand, req.userId];
+    await userService.setUserInit(initReq);
+    return res.status(200).json('success: ok');
+  } catch (error) {
+    console.error('Error during authentication:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
 const editProfile = async (req, res) => {
   try {
+    // middleware에서 req.userId으로 보낸 JWT payload
     await userService.patchUserProfile([req.body, req.userId]);
     return res.status(200).json('success : ok');
   } catch (error) {
@@ -53,27 +53,30 @@ const editProfile = async (req, res) => {
 };
 
 const getUserInfo = async (req, res) => {
-  const id = req.params.userId;
   try {
-    const userInfo = await userService.getUserInfo(id);
-    return userInfo && res.status(200).json({ success: ok, data: userInfo });
+    const userInfo = await userService.getUserInfo(req.userId);
+    return userInfo && res.status(200).json({ data: userInfo });
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
-// middleware 적용
-const test = async (req, res) => {
-  // middleware에서 req.[name]으로 보낸 JWT payload
-  await res.status(200).json(`${req.userId}, ${req.nickname}`);
+const checkUserNickname = async (req, res) => {
+  const { nickname } = req.query;
+  try {
+    const resault = await userService.checkUserNickname(nickname);
+    return res.status(200).json({ data: resault });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 };
 
 module.exports = {
-  test,
   signIn,
   kakaoRedirect,
   googleRedirect,
   setInitForm,
   getUserInfo,
-  editProfile
+  editProfile,
+  checkUserNickname
 };
