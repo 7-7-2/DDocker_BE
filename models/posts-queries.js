0a48e3c479d235f5e875ed22f379f36f13694426
hookor <gerrard1219@gmail.com>
2024-02-01 15:58:47 +0900

    fix: correct validation predicate

--- models/posts-queries.js
+++ models/posts-queries.js
@@ -1,38 +1,14 @@
-const buildInsert = (table, columns) => {
-  const replacement = '?, '.repeat(columns.length - 1) + '?';
-  return `INSERT INTO ${table} ( ${columns.join(
-    ','
-  )} ) VALUES ( ${replacement} )`;
-};
-const buildDelete = (table, params) => {
-  const predi = params.reduce(
-    (acc, cur, idx) => (acc += `${idx !== 0 ? ' AND ' : ''}` + `${cur} = ?`),
-    ''
-  );
-  return `DELETE FROM ${table} WHERE ${predi}`;
-};
+const query = require('./query-fns');
 
-const buildPatchQuery = async (postId, updateTo) => {
-  const joinedProps = Object.entries(updateTo).reduce(
-    (acc, [key, val], idx, arr) => {
-      const valueToSet = typeof updateTo[key] === 'string' ? `'${val}'` : val;
-      const optionalComma = idx < arr.length - 1 ? ',' : '';
-      return `${acc}${key}=${valueToSet}${optionalComma}`;
-    },
-    ''
-  );
-  const patchProps = Object.keys(updateTo).length !== 0 ? joinedProps : null;
-  return `UPDATE post SET ${patchProps} WHERE id = ${postId}`;
-};
-
+//TODO: SELECT 함수화
 const queries = {
   getPostDetail: `SELECT 
   a.brand, a.menu, a.post_title,size, a.shot,caffeine, a.photo, a.created_at, b.profileUrl, b.nickname, b.sum 
   FROM post a 
   LEFT JOIN user b 
-  ON a.user_id = b.id 
-  WHERE a.id=?`,
-  registerPost: buildInsert('post', [
+  ON a.user_id = b.public_id 
+  WHERE a.user_id=?`,
+  registerPost: query.buildInsert('post', [
     'user_id',
     'brand',
     'menu',
@@ -40,14 +16,33 @@ const queries = {
     'size',
     'shot',
     'caffeine',
-    'photo'
+    'photo',
+    'public_id'
   ]),
-  deletePost: buildDelete('post', ['id']),
-  writeComment: buildInsert('comment', ['user_id', 'post_id', 'content']),
-  deleteComment: buildDelete('comment', ['post_id', 'id']),
-  replyComment: buildInsert('reply', ['user_id', 'comment_id', 'content']),
-  deleteReply: buildDelete('reply', ['id']),
-  buildPatchQuery
+  deletePost: query.buildDelete('post', ['public_id']),
+  writeComment: query.buildInsert('comment', ['user_id', 'post_id', 'content']),
+  deleteComment: query.buildDelete('comment', ['post_id', 'id']),
+  replyComment: query.buildInsert('reply', [
+    'user_id',
+    'comment_id',
+    'content'
+  ]),
+  deleteReply: query.buildDelete('reply', ['id']),
+  getComments: `SELECT 
+  u.profileUrl, u.nickname, c.content, c.created_at 
+  FROM user u
+  LEFT JOIN comment c
+  ON c.post_id = ?
+  WHERE c.user_id = u.public_id
+  `,
+  getReply: `SELECT
+  u.profileUrl, u.nickname, r.content, r.created_at 
+  FROM user u
+  LEFT JOIN reply r
+  ON r.comment_id = ?
+  WHERE r.user_id = u.public_id
+  `,
+  buildPatchQuery: query.buildPatchQuery
 };
 
 module.exports = queries;
