f0cbc10e05c313cca092f33efe565505866690d3
swon95 <shimtkddnjs13@gmail.com>
2024-02-01 17:26:45 +0900

    feat: getCoffeeInfo

--- models/coffee-db.js
+++ models/coffee-db.js
@@ -5,13 +5,19 @@ exports.getCoffeeInfoSum = async ({ getReq }) => {
   const getConn = await conn.getConnection();
   const params = [getReq];
   const sql = `
-    SELECT brand, SUM(caffeine), COUNT(*)
+    SELECT brand, caffeine
     FROM post
     WHERE user_id = ? AND DATE(created_at) = CURRENT_DATE
-    `;
+  `;
   const [row] = await getConn.query(sql, params);
+  const sql2 = `
+    SELECT SUM(caffeine), count(*)
+    FROM post
+    WHERE user_id = ? AND DATE(created_at) = CURRENT_DATE
+  `;
+  const [sumRow] = await getConn.query(sql2, params);
   getConn.release();
-  return row;
+  return { sumData: sumRow, mapItem: row };
 };
 
 exports.getDaySum = async ({ getReq }) => {
@@ -29,9 +35,9 @@ exports.getDaySum = async ({ getReq }) => {
   };
   const getCoffeeInfoByMonth = async () => {
     const sql = `
-        SELECT COUNT(*) AS CountSum, SUM(caffeine) AS CaffeineSum
-        FROM post
-        WHERE user_id = ? AND MONTH(created_at) = MONTH(CURDATE())
+      SELECT COUNT(*), SUM(caffeine) AS CaffeineSum
+      FROM post
+      WHERE user_id = ? AND MONTH(created_at) = MONTH(CURDATE())
     `;
     const [row] = await getConn.query(sql, params);
     return row;
