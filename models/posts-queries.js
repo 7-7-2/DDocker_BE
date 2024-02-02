ba4474d2509e1d16ae7a92a87b4c1629010a3329
hookor <gerrard1219@gmail.com>
2024-02-02 17:17:01 +0900

    refactor: getFollowingPosts with likeCounts

--- models/posts-queries.js
+++ models/posts-queries.js
@@ -43,46 +43,56 @@ const queries = {
   WHERE r.user_id = u.public_id
   `,
   getFollowingPosts: `
+  WITH UserList AS (
+      SELECT a.public_id, a.profileUrl, a.nickname, a.sum
+      FROM user a
+      INNER JOIN (
+          SELECT b.followed_user_id
+          FROM follows b
+          LEFT JOIN user c ON c.public_id = b.following_user_id
+          WHERE c.public_id = ?
+      ) list ON list.followed_user_id = a.public_id
+  ),
+  TotalComments AS (
+      SELECT
+          co.post_id,
+          COUNT(DISTINCT co.id) AS total_comments
+      FROM comment co
+      GROUP BY co.post_id
+  ),
+  TotalReplies AS (
+      SELECT
+          comment.post_id,
+          COUNT(*) AS total_replies
+      FROM reply
+      INNER JOIN comment ON reply.comment_id = comment.id
+      GROUP BY comment.post_id
+  ),
+  LikeCounts AS (
+      SELECT post_id, COUNT(id) AS likeCounts
+      FROM likes
+      GROUP BY post_id
+  )
   SELECT
-    p.public_id,
-    p.post_title,
-    p.brand,
-    p.menu,
-    p.shot,
-    p.caffeine,
-    p.photo,
-    p.created_at,
-    u.profileUrl,
-    u.nickname,
-    u.sum,
-    COALESCE(SUM(COALESCE(com.total_comments, 0) + COALESCE(rep.total_replies, 0)), 0) AS total
-FROM post p
-INNER JOIN (
-    SELECT a.public_id, a.profileUrl, a.nickname, a.sum
-    FROM user a
-    INNER JOIN (
-        SELECT b.followed_user_id
-        FROM follows b
-        LEFT JOIN user c ON c.public_id = b.following_user_id
-        WHERE c.public_id = ?
-    ) list ON list.followed_user_id = a.public_id
-) u ON u.public_id = p.user_id
-LEFT JOIN (
-    SELECT
-        co.post_id,
-        COUNT(DISTINCT co.id) AS total_comments
-    FROM comment co
-    GROUP BY co.post_id
-) com ON com.post_id = p.public_id
-LEFT JOIN (
-    SELECT
-        comment.post_id,
-        COUNT(*) AS total_replies
-    FROM reply
-    INNER JOIN comment ON reply.comment_id = comment.id
-    GROUP BY comment.post_id
-) rep ON rep.post_id = com.post_id
-GROUP BY p.public_id, p.post_title, p.brand, p.menu, p.shot, p.caffeine, p.photo, p.created_at, u.profileUrl, u.nickname, u.sum;
+      p.public_id,
+      p.post_title,
+      p.brand,
+      p.menu,
+      p.shot,
+      p.caffeine,
+      p.photo,
+      p.created_at,
+      u.profileUrl,
+      u.nickname,
+      u.sum,
+      COALESCE(SUM(COALESCE(tc.total_comments, 0) + COALESCE(tr.total_replies, 0)), 0) AS totalComments,
+      lk.likeCounts
+  FROM post p
+  INNER JOIN UserList u ON u.public_id = p.user_id
+  LEFT JOIN TotalComments tc ON tc.post_id = p.public_id
+  LEFT JOIN TotalReplies tr ON tr.post_id = tc.post_id
+  LEFT JOIN LikeCounts lk ON lk.post_id = p.public_id
+  GROUP BY p.public_id, p.post_title, p.brand, p.menu, p.shot, p.caffeine, p.photo, p.created_at, u.profileUrl, u.nickname, u.sum, lk.likeCounts;
 `,
   buildPatchQuery: query.buildPatchQuery
 };
