24ba8a7763f84bba406771bdd169e89d05a7ce97
Siwoo Lee <siwoo28@kakao.com>
2024-02-02 15:05:55 +0900

    Refactor: status code 수정

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,94 +2,59 @@ const userService = require('../services/user-service');
 
 const signIn = async (req, res) => {
   const { social } = req.query;
-  try {
-    if (social === 'google') await userService.googleSignIn(res);
-    if (social === 'kakao') await userService.kakaoSignIn(res);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  if (social === 'google') await userService.googleSignIn(res);
+  if (social === 'kakao') await userService.kakaoSignIn(res);
 };
 
 const googleRedirect = async (req, res) => {
   const { code } = await req.query;
-  try {
-    const userInfo = await userService.getGoogleAuth(code);
-    const accessToken = await userService.setUserOauth(userInfo);
-    return res.status(200).json({ success: 'ok', accessToken: accessToken });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  const userInfo = await userService.getGoogleAuth(code);
+  const accessToken = await userService.setUserOauth(userInfo, res);
+  return accessToken && accessToken;
 };
 
 const kakaoRedirect = async (req, res) => {
   const { code } = await req.query;
-  try {
-    const userInfo = await userService.getKakaoAuth(code);
-    const accessToken = await userService.setUserOauth(userInfo);
-    return res.status(200).json({ success: 'ok', accessToken: accessToken });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  const userInfo = await userService.getKakaoAuth(code);
+  const accessToken = await userService.setUserOauth(userInfo, res);
+  return accessToken && accessToken;
 };
 
 const setInitForm = async (req, res) => {
-  try {
-    const body = await req.body;
-    const initReq = [body.nickName, body.gender, body.brand, req.userId];
-    await userService.setUserInit(initReq);
-    return res.status(200).json('success: ok');
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  const body = await req.body;
+  const initReq = [body.nickName, body.gender, body.brand, req.userId];
+  const result = await userService.setUserInit(initReq);
+  return result && res.status(201).json('success: Created');
 };
 
 const editProfile = async (req, res) => {
-  try {
-    await userService.patchUserProfile([req.body, req.userId]);
-    return res.status(200).json('success : ok');
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  const result = await userService.patchUserProfile([req.body, req.userId]);
+  return result && res.status(200).json('success : ok');
 };
 
 const getUserInfo = async (req, res) => {
-  try {
-    const userId = req.params.userId;
-    const userInfo = await userService.getUserInfo(userId);
-    return userInfo && res.status(200).json({ success: 'ok', data: userInfo });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  const userId = req.params.userId;
+  const userInfo = await userService.getUserInfo(userId);
+  return userInfo && res.status(200).json({ success: 'ok', data: userInfo });
 };
 
 const checkUserNickname = async (req, res) => {
   const { nickname } = req.query;
-  try {
-    const result = await userService.checkUserNickname(nickname);
-    return res.status(200).json({ success: 'ok', data: result });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  const result = await userService.checkUserNickname(nickname);
+  return result && res.status(200).json({ success: 'ok', data: result });
 };
 
 const getUserPosts = async (req, res) => {
-  try {
-    const userId = req.params.userId;
-    const posts = await userService.getUserPosts(userId);
-    return res.status(200).json({ success: 'ok', data: posts });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  const userId = req.params.userId;
+  const pages = req.params.pages;
+  const posts = await userService.getUserPosts([userId, pages]);
+  return posts && res.status(200).json({ success: 'ok', data: posts });
 };
 
 const getUserFollowsCount = async (req, res) => {
-  try {
-    const userId = req.params.userId;
-    const result = await userService.getUserFollowsCount(userId);
-    return res.status(200).json({ success: 'ok', data: result });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+  const userId = req.params.userId;
+  const result = await userService.getUserFollowsCount(userId);
+  return result && res.status(200).json({ success: 'ok', data: result });
 };
 
 module.exports = {
