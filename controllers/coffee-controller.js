5ccc29ba771eb0bbf27cf75e4a6dc7650cab9055
swon95 <shimtkddnjs13@gmail.com>
2024-02-02 15:03:15 +0900

    refactor: DBexecute

--- controllers/coffee-controller.js
+++ controllers/coffee-controller.js
@@ -21,18 +21,16 @@ module.exports = {
         message: err
       });
     });
-
     return res.status(200).json({ setRes });
   },
 
   getCalendar: async (req, res) => {
     const getReq = req.params.userId;
-    try {
-      const setRes = await coffeeService.getCalendar({ getReq });
-      res.status(200).json({ setRes });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
+    const setRes = await coffeeService.getCalendar({ getReq }).catch(err => {
+      res.status(500).json({
+        message: err
+      });
+    });
+    return res.status(200).json({ setRes });
   }
 };
--- models/coffee-db.js
+++ models/coffee-db.js
@@ -1,68 +1,48 @@
 const db = require('../loaders/db');
 
-exports.getCoffeeInfoSum = async ({ getReq }) => {
+const executeQuery = async (sql, params) => {
   const conn = await db();
   const getConn = await conn.getConnection();
+  const [row] = await getConn.query(sql, params);
+  getConn.release();
+  return row;
+};
+
+exports.getCoffeeInfoSum = async ({ getReq }) => {
   const params = [getReq];
   const sql = `
     SELECT brand, caffeine
     FROM post
     WHERE user_id = ? AND DATE(created_at) = CURRENT_DATE
   `;
-  const [row] = await getConn.query(sql, params);
+  const [mapItem] = await executeQuery(sql, params);
   const sql2 = `
     SELECT SUM(caffeine), count(*)
     FROM post
     WHERE user_id = ? AND DATE(created_at) = CURRENT_DATE
   `;
-  const [sumRow] = await getConn.query(sql2, params);
-  getConn.release();
-  return { sumData: sumRow, mapItem: row };
+  const [sumRow] = await executeQuery(sql2, params);
+  return { sumRow, mapItem };
 };
 
 exports.getDaySum = async ({ getReq }) => {
-  const conn = await db();
-  const getConn = await conn.getConnection();
   const params = [getReq];
-  const getCoffeeInfoByYear = async () => {
+  const branchFuc = async date => {
     const sql = `
-        SELECT COUNT(*) AS CountSum, SUM(caffeine) AS CaffeineSum
-        FROM post
-        WHERE user_id = ? AND YEAR(created_at) = YEAR(CURDATE())
-    `;
-    const [row] = await getConn.query(sql, params);
-    return row;
+          SELECT COUNT(*) AS CountSum, SUM(caffeine) AS CaffeineSum
+          FROM post
+          WHERE user_id = ? ${date}
+      `;
+    return await executeQuery(sql, params);
   };
-  const getCoffeeInfoByMonth = async () => {
-    const sql = `
-      SELECT COUNT(*), SUM(caffeine) AS CaffeineSum
-      FROM post
-      WHERE user_id = ? AND MONTH(created_at) = MONTH(CURDATE())
-    `;
-    const [row] = await getConn.query(sql, params);
-    return row;
-  };
-  const getCoffeeInfoByWeek = async () => {
-    const sql = `
-        SELECT COUNT(*) AS CountSum, SUM(caffeine) AS CaffeineSum
-        FROM post
-        WHERE user_id = ? AND WEEK(created_at) = WEEK(CURDATE())
-    `;
-    const [row] = await getConn.query(sql, params);
-    return row;
-  };
+  const year = await branchFuc(`AND YEAR(created_at) = YEAR(CURDATE())`);
+  const month = await branchFuc(`AND MONTH(created_at) = MONTH(CURDATE())`);
+  const week = await branchFuc(`AND WEEK(created_at) = WEEK(CURDATE())`);
 
-  const resultByYear = await getCoffeeInfoByYear({ getReq });
-  const resultByMonth = await getCoffeeInfoByMonth({ getReq });
-  const resultByWeek = await getCoffeeInfoByWeek({ getReq });
-
-  getConn.release();
-  return { resultByYear, resultByMonth, resultByWeek };
+  return { year, month, week };
 };
 
 exports.getCalendar = async ({ getReq }) => {
-  const conn = await db();
-  const getConn = await conn.getConnection();
   const params = [getReq];
   const sql = `
         SELECT DAY(created_at) as day, sum(caffeine) as CaffeineSum
@@ -70,7 +50,5 @@ exports.getCalendar = async ({ getReq }) => {
         WHERE user_id = ? AND YEAR(created_at) = YEAR(CURRENT_DATE) AND MONTH(created_at) = MONTH   (CURRENT_DATE)
         GROUP BY DAY(created_at)
     `;
-  const [row] = await getConn.query(sql, params);
-  getConn.release();
-  return row;
+  return await executeQuery(sql, params);
 };
