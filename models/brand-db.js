8598da683cfe0be5fb1abac5207f80d156ed5f9c
Siwoo Lee <siwoo28@kakao.com>
2024-02-27 22:12:51 +0900

    Fix: getBrandData

--- models/brand-db.js
+++ models/brand-db.js
@@ -2,11 +2,19 @@ const db = require('../loaders/db');
 
 module.exports = {
   getBrandData: async () => {
-    const connection = await db();
-    const [rows, fields] = await connection.query(
-      'SELECT brand_name, name, caffeine FROM brand'
-    );
-    connection.end();
-    return rows;
+    const sql = `SELECT JSON_OBJECTAGG(brand_name, brand_items) AS coffee_menus
+    FROM (
+        SELECT brand_name, JSON_ARRAYAGG(
+            JSON_OBJECT('brand', brand_name, 'caffeine', caffeine, 'menu', name)
+        ) AS brand_items
+        FROM brand
+        GROUP BY brand_name
+    ) AS temp`;
+    const conn = await db();
+    const getConn = await conn.getConnection();
+    const result = await getConn.query(sql).catch(err => console.log(err));
+    getConn.release();
+    console.log(result[0]);
+    return result[0] ? result[0] : null;
   }
 };
--- services/brand-service.js
+++ services/brand-service.js
@@ -1,7 +1,7 @@
 const brandDB = require('../models/brand-db');
 
 module.exports = {
-  getBrandData: async (req, res) => {
+  getBrandData: async req => {
     const result = await brandDB.getBrandData(req);
     return result ? result : Promise.reject('Failed to get brandData');
   }
