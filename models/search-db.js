cd1e4d41d12360454fe472246fdeaa2c1c342450
hookor <gerrard1219@gmail.com>
2024-03-22 00:00:30 +0900

    refactor: search for debounced search

--- models/search-db.js
+++ models/search-db.js
@@ -1,15 +1,16 @@
 const db = require('../loaders/db');
 
-exports.getSearchList = async ({ getReq }) => {
+exports.getSearchList = async ({ searchReq }) => {
   const conn = await db();
   const getConn = await conn.getConnection();
   const sql = `
-    SELECT profileUrl, nickname, sum
+    SELECT profileUrl as url, nickname, sum as caffeine, public_id as userId
     FROM user
-    WHERE nickname LIKE ?
+    WHERE LOWER(nickname) LIKE LOWER(?) LIMIT 5
     `;
-  const params = [`%${getReq}%`];
-  const [row] = await getConn.query(sql, params);
+  const params = [`%${searchReq}%`];
+  const result = await getConn.query(sql, params);
+  const data = result[0];
   getConn.release();
-  return row;
+  return data;
 };
