ca61d6453cfff87ca2d3356c5c93139cabaa8f04
hookor <gerrard1219@gmail.com>
2024-03-28 17:16:29 +0900

    chore: remove swagger comments in routes

--- routes/search-route.js
+++ routes/search-route.js
@@ -6,13 +6,7 @@ const SearchController = require('../controllers/search-controller');
 router.get(
   '/search/:nickname',
   errorHandler(SearchController.getUserInfo)
-  // #swagger.tags = ['SEARCH']
-  // #swagger.summary = '유저 검색'
-  // #swagger.responses[200] = { description: 'OK' }
-  // #swagger.responses[400] = { description: 'Bad Request' }
-  // #swagger.responses[500] = { description: 'Internal Server Error' }
 );
 
 module.exports = router;
 
-// routes => 서버에 요청이 들어올 때 URI의 path에 따라 필요한 controller로 이어주는 역할만 담당
