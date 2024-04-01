06d3a1869e3b2450ccd6b6a51a67855bea94a7e8
hookor <gerrard1219@gmail.com>
2024-04-02 01:15:59 +0900

    feat: getSearchListMore

--- services/search-service.js
+++ services/search-service.js
@@ -4,5 +4,9 @@ module.exports = {
   getUserInfo: async (req, res) => {
     const result = await searchDB.getSearchList(req);
     return result ? result : Promise.reject('Failed to get userInfo');
+  },
+  getUserInfoMore: async (req, res) => {
+    const result = await searchDB.getSearchListMore(req);
+    return result ? result : Promise.reject('Failed to get userInfoMore');
   }
 };
