c8565eba56517c572e4eb109e47fcc07cb73a0ba
Siwoo Lee <siwoo28@kakao.com>
2024-05-28 18:28:14 +0900

    Feat: UnlinkSocialAuth API

--- routes/users-route.js
+++ routes/users-route.js
@@ -4,37 +4,51 @@ const AuthMiddleware = require('../middlewares/authMiddleware');
 const errorHandler = require('../middlewares/errorHandler');
 const userController = require('../controllers/user-controller');
 
-router.get('/users/signIn', errorHandler(userController.signIn));
-
+// 소셜 로그인
 router.get(
-  '/users/google/redirect',
-  errorHandler(userController.googleRedirect)
+  '/users/signIn/:social/:unlink?',
+  errorHandler(userController.signIn)
 );
 
-router.get('/users/kakao/redirect', errorHandler(userController.kakaoRedirect));
+// DDOCKER 로그인 & 회원가입 판별
+router.get(
+  '/users/:social/redirect',
+  errorHandler(userController.socialRedirect)
+);
 
+// 소셜 & DDOCKER 연동 해제
+router.get(
+  '/users/:social/unlink/:token',
+  errorHandler(userController.unlinkSocialAuth)
+);
+
+// DDOCKER 회원탛퇴
+router.delete(
+  '/users/:social',
+  AuthMiddleware.verifyToken,
+  errorHandler(userController.deleteAccount)
+);
+
+// 닉네임 중복체크
 router.get('/users/check', errorHandler(userController.checkUserNickname));
 
-router
-  .route('/users')
-  .post(AuthMiddleware.verifyToken, errorHandler(userController.setInitForm))
-  .delete(
-    AuthMiddleware.verifyToken,
-    errorHandler(userController.deleteAccount)
-  );
+// DDOCKER 회원가입
+router.post('/users', errorHandler(userController.setInitForm));
 
+// DDOCKER 개인 프로필 조회 & 편집
 router
   .route('/users/userInfo')
   .get(AuthMiddleware.verifyToken, errorHandler(userController.getUserInfo))
   .patch(AuthMiddleware.verifyToken, errorHandler(userController.editProfile));
 
+// DDOCKER 타 사용자 정보 조회
 router.get('/users/:userId/userInfo', errorHandler(userController.getUserInfo));
 
+// 사용자 별 PROFILE PAGE 데이터 조회
 router.get(
   '/users/:userId/follow',
   errorHandler(userController.getUserFollowsCount)
 );
-
 router.get(
   '/users/:userId/posts/:pages',
   errorHandler(userController.getUserPosts)
