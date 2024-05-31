fbbc5416585cf70a907ff439b6aa5f7b77b015f0
hookor <gerrard1219@gmail.com>
2024-05-31 16:15:06 +0900

    feat: add new brands in brandMap

--- middlewares/switchBrand.js
+++ middlewares/switchBrand.js
@@ -8,7 +8,11 @@ brandMap.set('theventi', '더벤티');
 brandMap.set('megacoffee', '메가커피');
 brandMap.set('compose', '컴포즈커피');
 brandMap.set('pascucci', '파스쿠찌');
-brandMap.set('bbak', '빽다방');
+brandMap.set('tomntoms', '탐앤탐스');
+brandMap.set('coffeebean', '커피빈');
+brandMap.set('mammoth', '매머드커피');
+brandMap.set('private', '나만의 카페');
+brandMap.set('banapresso', '바나프레소');
 
 const switchBrand = brand => {
   return brandMap.get(brand);
